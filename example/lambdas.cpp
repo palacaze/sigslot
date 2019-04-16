29d0e69c54666c43ea6bd734e6f415a12d591e53
Pierre-Antoine Lacaze <pa.lacaze@gmail.com>
2019-04-16 23:37:51 +0200

    Add an example to track inappropriate build times

---
+++ example/lambdas.cpp
@@ -0,0 +1,23 @@
+#include <sigslot/signal.hpp>
+#include <cstdio>
+#include <functional>
+
+/*
+ * This example is meant to test compilation time as well as object size
+ * when a lot of unique callables get connected to a signal.
+ * This example motivated the introduction of a REDUCE_CODE_SIZE cmake option.
+ */
+
+#define LAMBDA4() sig.connect([](int) { puts("lambda\n"); });
+#define LAMBDA3() LAMBDA4() LAMBDA4() LAMBDA4() LAMBDA4() LAMBDA4()
+#define LAMBDA2() LAMBDA3() LAMBDA3() LAMBDA3() LAMBDA3() LAMBDA3()
+#define LAMBDA1() LAMBDA2() LAMBDA2() LAMBDA2() LAMBDA2() LAMBDA2()
+#define LAMBDAS() LAMBDA1() LAMBDA1() LAMBDA1() LAMBDA1() LAMBDA1()
+
+int main() {
+    sigslot::signal<int> sig;
+
+    // connect a bunch of lambdas
+    LAMBDAS()
+    return 0;
+}
